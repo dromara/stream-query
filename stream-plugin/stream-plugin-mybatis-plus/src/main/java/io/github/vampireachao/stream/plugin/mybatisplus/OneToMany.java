5d0c7d65eacde7b4dbd431478f5bdcb471106c01
VampireAchao <achao1441470436@gmail.com>
2022-05-23 18:02:34 +0800

    one to many

---
+++ stream-plugin/stream-plugin-mybatis-plus/src/main/java/io/github/vampireachao/stream/plugin/mybatisplus/OneToMany.java
@@ -0,0 +1,139 @@
+package io.github.vampireachao.stream.plugin.mybatisplus;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.support.SFunction;
+import com.baomidou.mybatisplus.extension.toolkit.SimpleQuery;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Consumer;
+import java.util.function.UnaryOperator;
+import java.util.stream.Collectors;
+
+/**
+ * 一对多
+ *
+ * @author <achao1441470436@gmail.com>
+ * @since 2022/5/23 17:40
+ */
+public class OneToMany {
+
+    private OneToMany() {
+        /* Do not new me! */
+    }
+
+    // dataList key
+
+    @SafeVarargs
+    public static <K extends Serializable, T> Map<K, List<T>> query(Collection<K> dataList, SFunction<T, K> keyFunction, Consumer<T>... peeks) {
+        return query(UnaryOperator.identity(), dataList, keyFunction, false, peeks);
+    }
+
+    // wrapper dataList key
+
+    @SafeVarargs
+    public static <K extends Serializable, T> Map<K, List<T>> query(UnaryOperator<LambdaQueryWrapper<T>> queryOperator, Collection<K> dataList, SFunction<T, K> keyFunction, Consumer<T>... peeks) {
+        return query(queryOperator, dataList, keyFunction, false, peeks);
+    }
+
+    // dataList key parallel
+
+    @SafeVarargs
+    public static <K extends Serializable, T> Map<K, List<T>> query(Collection<K> dataList, SFunction<T, K> keyFunction, boolean isParallel, Consumer<T>... peeks) {
+        return query(UnaryOperator.identity(), dataList, keyFunction, isParallel, peeks);
+    }
+
+    // wrapper dataList key parallel
+
+    @SafeVarargs
+    public static <K extends Serializable, T> Map<K, List<T>> query(UnaryOperator<LambdaQueryWrapper<T>> queryOperator, Collection<K> dataList, SFunction<T, K> keyFunction, boolean isParallel, Consumer<T>... peeks) {
+        return QueryHelper.lambdaQuery(dataList, keyFunction).map(queryOperator).map(wrapper -> SimpleQuery.group(wrapper, keyFunction, isParallel, peeks)).orElseGet(HashMap::new);
+    }
+
+    // dataList key value
+
+    @SafeVarargs
+    public static <K extends Serializable, V, T> Map<K, List<V>> query(Collection<K> dataList, SFunction<T, K> keyFunction, SFunction<T, V> valueFunction, Consumer<T>... peeks) {
+        return query(UnaryOperator.identity(), dataList, keyFunction, valueFunction, false, peeks);
+    }
+
+    // wrapper dataList key value
+
+    @SafeVarargs
+    public static <K extends Serializable, V, T> Map<K, List<V>> query(UnaryOperator<LambdaQueryWrapper<T>> queryOperator, Collection<K> dataList, SFunction<T, K> keyFunction, SFunction<T, V> valueFunction, Consumer<T>... peeks) {
+        return query(queryOperator, dataList, keyFunction, valueFunction, false, peeks);
+    }
+
+    // dataList key value parallel
+
+    @SafeVarargs
+    public static <K extends Serializable, V, T> Map<K, List<V>> query(Collection<K> dataList, SFunction<T, K> keyFunction, SFunction<T, V> valueFunction, boolean isParallel, Consumer<T>... peeks) {
+        return query(UnaryOperator.identity(), dataList, keyFunction, valueFunction, isParallel, peeks);
+    }
+
+    // wrapper dataList key value parallel
+
+    @SafeVarargs
+    public static <K extends Serializable, V, T> Map<K, List<V>> query(UnaryOperator<LambdaQueryWrapper<T>> queryOperator, Collection<K> dataList, SFunction<T, K> keyFunction, SFunction<T, V> valueFunction, boolean isParallel, Consumer<T>... peeks) {
+        return QueryHelper.lambdaQuery(dataList, keyFunction).map(queryOperator.compose(w -> QueryHelper.select(w, keyFunction, valueFunction))).map(wrapper -> SimpleQuery.group(wrapper, keyFunction, Collectors.mapping(valueFunction, Collectors.toList()), isParallel, peeks)).orElseGet(HashMap::new);
+    }
+
+    // data key
+
+    @SafeVarargs
+    public static <K extends Serializable, T> Map<K, List<T>> query(K data, SFunction<T, K> keyFunction, Consumer<T>... peeks) {
+        return query(UnaryOperator.identity(), data, keyFunction, false, peeks);
+    }
+
+    // wrapper data key
+
+    @SafeVarargs
+    public static <K extends Serializable, T> Map<K, List<T>> query(UnaryOperator<LambdaQueryWrapper<T>> queryOperator, K data, SFunction<T, K> keyFunction, Consumer<T>... peeks) {
+        return query(queryOperator, data, keyFunction, false, peeks);
+    }
+
+    // data key parallel
+
+    @SafeVarargs
+    public static <K extends Serializable, T> Map<K, List<T>> query(K data, SFunction<T, K> keyFunction, boolean isParallel, Consumer<T>... peeks) {
+        return query(UnaryOperator.identity(), data, keyFunction, isParallel, peeks);
+    }
+
+    // wrapper data key parallel
+
+    @SafeVarargs
+    public static <K extends Serializable, T> Map<K, List<T>> query(UnaryOperator<LambdaQueryWrapper<T>> queryOperator, K data, SFunction<T, K> keyFunction, boolean isParallel, Consumer<T>... peeks) {
+        return QueryHelper.lambdaQuery(data, keyFunction).map(queryOperator).map(wrapper -> SimpleQuery.group(wrapper, keyFunction, isParallel, peeks)).orElseGet(HashMap::new);
+    }
+
+    // data key value
+
+    @SafeVarargs
+    public static <K extends Serializable, V, T> Map<K, List<V>> query(K data, SFunction<T, K> keyFunction, SFunction<T, V> valueFunction, Consumer<T>... peeks) {
+        return query(UnaryOperator.identity(), data, keyFunction, valueFunction, false, peeks);
+    }
+
+    // wrapper data key value
+
+    @SafeVarargs
+    public static <K extends Serializable, V, T> Map<K, List<V>> query(UnaryOperator<LambdaQueryWrapper<T>> queryOperator, K data, SFunction<T, K> keyFunction, SFunction<T, V> valueFunction, Consumer<T>... peeks) {
+        return query(queryOperator, data, keyFunction, valueFunction, false, peeks);
+    }
+
+    // data key value parallel
+
+    @SafeVarargs
+    public static <K extends Serializable, V, T> Map<K, List<V>> query(K data, SFunction<T, K> keyFunction, SFunction<T, V> valueFunction, boolean isParallel, Consumer<T>... peeks) {
+        return query(UnaryOperator.identity(), data, keyFunction, valueFunction, isParallel, peeks);
+    }
+
+    // wrapper data key value parallel
+
+    @SafeVarargs
+    public static <K extends Serializable, V, T> Map<K, List<V>> query(UnaryOperator<LambdaQueryWrapper<T>> queryOperator, K data, SFunction<T, K> keyFunction, SFunction<T, V> valueFunction, boolean isParallel, Consumer<T>... peeks) {
+        return QueryHelper.lambdaQuery(data, keyFunction).map(queryOperator.compose(w -> QueryHelper.select(w, keyFunction, valueFunction))).map(wrapper -> SimpleQuery.group(wrapper, keyFunction, Collectors.mapping(valueFunction, Collectors.toList()), isParallel, peeks)).orElseGet(HashMap::new);
+    }
+}
---
+++ stream-plugin/stream-plugin-mybatis-plus/src/test/java/io/github/vampireachao/stream/plugin/mybatisplus/OneToManyTest.java
@@ -0,0 +1,21 @@
+package io.github.vampireachao.stream.plugin.mybatisplus;
+
+import com.baomidou.mybatisplus.test.autoconfigure.MybatisPlusTest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * 一对多测试
+ *
+ * @author <achao1441470436@gmail.com>
+ * @since 2022/5/23 13:51
+ */
+@MybatisPlusTest
+class OneToManyTest {
+    @Test
+    void testQuery() {
+        Assertions.assertAll(() -> {
+
+        });
+    }
+}
