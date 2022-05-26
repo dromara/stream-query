75168ee8687aed58d91a1db12480750b1d70cb7f
VampireAchao <achao1441470436@gmail.com>
2022-05-26 12:59:42 +0800

    fix ddl

--- stream-plugin/stream-plugin-mybatis-plus/src/test/resources/schema.sql
+++ stream-plugin/stream-plugin-mybatis-plus/src/test/resources/schema.sql
@@ -1,63 +1,27 @@
 drop table if exists user_info;
 create table if not exists user_info
 (
-    id BIGINT
-(
-    20
-) NOT NULL COMMENT '主键ID',
-    name VARCHAR
-(
-    30
-) NULL DEFAULT NULL COMMENT '姓名',
-    age INT
-(
-    11
-) NULL DEFAULT NULL COMMENT '年龄',
-    email VARCHAR
-(
-    50
-) NULL DEFAULT NULL COMMENT '邮箱',
-    PRIMARY KEY
-(
-    id
-)
-    );
+    id    BIGINT(20) AUTO_INCREMENT NOT NULL COMMENT '主键ID',
+    name  VARCHAR(30)               NULL DEFAULT NULL COMMENT '姓名',
+    age   INT(11)                   NULL DEFAULT NULL COMMENT '年龄',
+    email VARCHAR(50)               NULL DEFAULT NULL COMMENT '邮箱',
+    PRIMARY KEY (id)
+);
 
 drop table if exists user_role;
 create table if not exists user_role
 (
-    id BIGINT
-(
-    20
-) NOT NULL COMMENT '主键ID',
-    user_id BIGINT
-(
-    20
-) NULL DEFAULT NULL COMMENT '用户ID',
-    role_id BIGINT
-(
-    20
-) NULL DEFAULT NULL COMMENT '角色ID',
-    PRIMARY KEY
-(
-    id
-)
-    );
+    id      BIGINT(20) AUTO_INCREMENT NOT NULL COMMENT '主键ID',
+    user_id BIGINT(20)                NULL DEFAULT NULL COMMENT '用户ID',
+    role_id BIGINT(20)                NULL DEFAULT NULL COMMENT '角色ID',
+    PRIMARY KEY (id)
+);
 
 
 drop table if exists role_info;
 create table if not exists role_info
 (
-    id BIGINT
-(
-    20
-) NOT NULL COMMENT '主键ID',
-    role_name VARCHAR
-(
-    30
-) NULL DEFAULT NULL COMMENT '角色名称',
-    PRIMARY KEY
-(
-    id
-)
-    );
+    id        BIGINT(20) AUTO_INCREMENT NOT NULL COMMENT '主键ID',
+    role_name VARCHAR(30)               NULL DEFAULT NULL COMMENT '角色名称',
+    PRIMARY KEY (id)
+);
