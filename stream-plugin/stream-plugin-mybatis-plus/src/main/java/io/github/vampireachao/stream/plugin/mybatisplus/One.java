55862bd78b7a04fa7a93bf96ed9ba72b5cb3713d
VampireAchao <achao1441470436@gmail.com>
2022-06-18 21:00:37 +0800

    One

---
+++ stream-plugin/stream-plugin-mybatis-plus/src/main/java/io/github/vampireachao/stream/plugin/mybatisplus/One.java
@@ -0,0 +1,48 @@
+package io.github.vampireachao.stream.plugin.mybatisplus;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.support.SFunction;
+import com.baomidou.mybatisplus.extension.toolkit.SimpleQuery;
+import com.baomidou.mybatisplus.extension.toolkit.SqlHelper;
+
+import java.io.Serializable;
+import java.util.Optional;
+import java.util.function.UnaryOperator;
+
+/**
+ * @author VampireAchao
+ * @since 2022/6/18 14:47
+ */
+public class One {
+
+    private One() {
+        /* Do not new me! */
+    }
+
+    // data key
+
+    public static <$KEY extends Serializable, $ENTITY> $ENTITY query($KEY data, SFunction<$ENTITY, $KEY> keyFunction) {
+        return query(UnaryOperator.identity(), data, keyFunction);
+    }
+
+    // data key value
+
+    public static <$KEY extends Serializable, $VALUE, $ENTITY> $VALUE query($KEY data, SFunction<$ENTITY, $KEY> keyFunction, SFunction<$ENTITY, $VALUE> valueFunction) {
+        return query(UnaryOperator.identity(), data, keyFunction, valueFunction);
+    }
+
+    // wrapper data key
+
+    public static <$KEY extends Serializable, $ENTITY> $ENTITY query(UnaryOperator<LambdaQueryWrapper<$ENTITY>> queryOperator, $KEY data, SFunction<$ENTITY, $KEY> keyFunction) {
+        return query(queryOperator, data, keyFunction, null);
+    }
+
+    // wrapper data key value
+
+    @SuppressWarnings("unchecked")
+    public static <$KEY extends Serializable, $VALUE, $ENTITY> $VALUE query(UnaryOperator<LambdaQueryWrapper<$ENTITY>> queryOperator, $KEY data, SFunction<$ENTITY, $KEY> keyFunction, SFunction<$ENTITY, $VALUE> valueFunction) {
+        return QueryHelper.lambdaQuery(data, keyFunction).map(queryOperator.compose(w -> QueryHelper.select(w, keyFunction, valueFunction))).map(wrapper -> SqlHelper.execute(SimpleQuery.getType(keyFunction), m -> m.selectOne(wrapper))).map(Optional.ofNullable(valueFunction).orElse(i -> ($VALUE) i)).orElse(null);
+    }
+
+
+}
---
+++ stream-plugin/stream-plugin-mybatis-plus/src/test/java/io/github/vampireachao/stream/plugin/mybatisplus/OneTest.java
@@ -0,0 +1,25 @@
+package io.github.vampireachao.stream.plugin.mybatisplus;
+
+import com.baomidou.mybatisplus.test.autoconfigure.MybatisPlusTest;
+import io.github.vampireachao.stream.plugin.mybatisplus.pojo.po.UserInfo;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * 查询单条测试
+ *
+ * @author VampireAchao
+ * @since 2022/6/18 14:25
+ */
+@MybatisPlusTest
+class OneTest {
+
+
+    @Test
+    void testQuery() {
+        Assertions.assertNotNull(One.query(1L, UserInfo::getId));
+        Assertions.assertNotNull(One.query(1L, UserInfo::getId, UserInfo::getName));
+        Assertions.assertNotNull(One.query(w -> w.le(UserInfo::getAge, 20), 1L, UserInfo::getId, UserInfo::getName));
+    }
+
+}
