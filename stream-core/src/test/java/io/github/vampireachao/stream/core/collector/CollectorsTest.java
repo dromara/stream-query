4145d76740f0ff06247a26de3cdd723c3680b785
VampireAchao <achao1441470436@gmail.com>
2022-07-03 21:46:00 +0800

    :trollface:

--- stream-core/src/test/java/io/github/vampireachao/stream/core/collector/CollectorsTest.java
+++ stream-core/src/test/java/io/github/vampireachao/stream/core/collector/CollectorsTest.java
@@ -3,6 +3,8 @@ package io.github.vampireachao.stream.core.collector;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.*;
+import java.util.stream.Collector;
 import java.util.stream.Stream;
 
 /**
@@ -17,6 +19,34 @@ class CollectorsTest {
     void testToMap() {
         Assertions.assertNull(Stream.of(null, null, null).collect(Collectors.toMap(Object::hashCode, Object::hashCode)).get(null));
         Assertions.assertArrayEquals(new Object[]{}, Stream.of(null, null, null).collect(Collectors.groupingBy(Object::hashCode)).get(null).toArray());
+    }
+
+
+    @Test
+    void testReducing() {
+        Set<Map<String, Integer>> nameScoreMapList = Stream.of(
+                new HashMap<String, Integer>() {{
+                    put("苏格拉底", 1);
+                    put("特拉叙马霍斯", 3);
+                }},
+                Collections.singletonMap("苏格拉底", 2),
+                Collections.singletonMap("特拉叙马霍斯", 1),
+                Collections.singletonMap("特拉叙马霍斯", 2)
+        ).collect(java.util.stream.Collectors.toSet());
+        Collector<Map<String, Integer>, ?, Map<String, List<Integer>>> reducing = Collectors.reducing(new HashMap<>(),
+                value -> {
+                    Map<String, List<Integer>> result = new HashMap<>();
+                    value.forEach((k, v) -> result.computeIfAbsent(k, i -> new ArrayList<>()).add(v));
+                    return result;
+                }, (l, r) -> {
+                    r.forEach((k, v) -> l.computeIfAbsent(k, i -> new ArrayList<>()).addAll(v));
+                    return l;
+                }
+        );
+        Assertions.assertEquals(new HashMap<String, List<Integer>>() {{
+            put("苏格拉底", Arrays.asList(1, 2));
+            put("特拉叙马霍斯", Arrays.asList(3, 1, 2));
+        }}, nameScoreMapList.stream().collect(reducing));
     }
 
 }
