09a586eed430e369e24c500232d3435be438ab62
VampireAchao <achao1441470436@gmail.com>
2022-07-15 15:33:04 +0800

    :trollface: static import

--- stream-core/src/test/java/io/github/vampireachao/stream/core/collector/CollectorsTest.java
+++ stream-core/src/test/java/io/github/vampireachao/stream/core/collector/CollectorsTest.java
@@ -7,6 +7,8 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 
+import static io.github.vampireachao.stream.core.collector.Collectors.*;
+
 /**
  * Collectors测试
  *
@@ -17,18 +19,18 @@ class CollectorsTest {
 
     @Test
     void testToMap() {
-        Assertions.assertNull(Stream.of(null, null, null).collect(Collectors.toMap(Object::hashCode, Object::hashCode)).get(null));
+        Assertions.assertNull(Stream.of(null, null, null).collect(toMap(Object::hashCode, Object::hashCode)).get(null));
     }
 
     @Test
     void testGroupingBy() {
-        Assertions.assertArrayEquals(new Object[]{}, Stream.of(null, null, null).collect(Collectors.groupingBy(Object::hashCode)).get(null).toArray());
+        Assertions.assertArrayEquals(new Object[]{}, Stream.of(null, null, null).collect(groupingBy(Object::hashCode)).get(null).toArray());
     }
 
     @Test
     void testFlatMapping() {
         List<Integer> actual = Stream.iterate(0, i -> ++i).limit(3)
-                .collect(Collectors.flatMapping(i -> Stream.of(i, i), Collectors.toList()));
+                .collect(flatMapping(i -> Stream.of(i, i), toList()));
         Assertions.assertEquals(Arrays.asList(0, 0, 1, 1, 2, 2), actual);
     }
 
