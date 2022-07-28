7a6313098d935d91e288466f0f2e8b96fb04c810
韩帅 <shuai.han@ersoft.cn>
2022-07-28 10:58:13 +0800

    insertBatch

--- stream-plugin/stream-plugin-mybatis-plus/src/main/java/io/github/vampireachao/stream/plugin/mybatisplus/injector/IMapper.java
+++ stream-plugin/stream-plugin-mybatis-plus/src/main/java/io/github/vampireachao/stream/plugin/mybatisplus/injector/IMapper.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.Collection;
+import java.util.List;
 
 import static io.github.vampireachao.stream.plugin.mybatisplus.injector.SqlInjectorConfig.COLLECTION_PARAM_NAME;
 
@@ -19,5 +20,13 @@ public interface IMapper<T> extends BaseMapper<T> {
      * @return 条数
      */
     long insertOneSql(@Param(COLLECTION_PARAM_NAME) Collection<T> list);
+
+    /**
+     * 批量新增（mysql语法批量）
+     *
+     * @param batchList 批量数据
+     * @return 结果
+     */
+    int insertBatch(@Param("items") List<T> batchList);
 
 }
