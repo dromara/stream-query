82f547316ab48482f0a14818478339ee56b5e744
VampireAchao <achao1441470436@gmail.com>
2022-07-29 15:02:48 +0800

    :trollface: insertFewSql

--- stream-plugin/stream-plugin-mybatis-plus/src/main/java/io/github/vampireachao/stream/plugin/mybatisplus/engine/mapper/IMapper.java
+++ stream-plugin/stream-plugin-mybatis-plus/src/main/java/io/github/vampireachao/stream/plugin/mybatisplus/engine/mapper/IMapper.java
@@ -29,8 +29,8 @@ public interface IMapper<T> extends BaseMapper<T> {
      * @param list 集合
      * @return 是否成功
      */
-    default boolean insertManySql(Collection<T> list) {
-        return this.insertManySql(list, PluginConst.DEFAULT_BATCH_SIZE);
+    default boolean insertFewSql(Collection<T> list) {
+        return this.insertFewSql(list, PluginConst.DEFAULT_BATCH_SIZE);
     }
 
     /**
@@ -40,7 +40,7 @@ public interface IMapper<T> extends BaseMapper<T> {
      * @param batchSize 分割量
      * @return 是否成功
      */
-    default boolean insertManySql(Collection<T> list, int batchSize) {
+    default boolean insertFewSql(Collection<T> list, int batchSize) {
         return Steam.of(list).subList(batchSize).allMatch(l -> this.insertOneSql(l) == l.size());
     }
 }
