74a2184e2b1f18bf0f81053df2ef89451c2d37e4
VampireAchao <achao1441470436@gmail.com>
2022-08-18 21:20:06 +0800

    :trollface: sleepy

--- stream-plugin/stream-plugin-mybatis-plus/src/main/java/io/github/vampireachao/stream/plugin/mybatisplus/engine/methods/UpdateOneSql.java
+++ stream-plugin/stream-plugin-mybatis-plus/src/main/java/io/github/vampireachao/stream/plugin/mybatisplus/engine/methods/UpdateOneSql.java
@@ -3,7 +3,9 @@ package io.github.vampireachao.stream.plugin.mybatisplus.engine.methods;
 import com.baomidou.mybatisplus.core.injector.AbstractMethod;
 import com.baomidou.mybatisplus.core.metadata.TableInfo;
 import io.github.vampireachao.stream.plugin.mybatisplus.engine.constant.PluginConst;
+import io.github.vampireachao.stream.plugin.mybatisplus.engine.enumration.SqlMethodEnum;
 import org.apache.ibatis.mapping.MappedStatement;
+import org.apache.ibatis.mapping.SqlSource;
 
 /**
  * 修改多条数据（mysql语法批量）
@@ -26,7 +28,17 @@ public class UpdateOneSql extends AbstractMethod implements PluginConst {
      */
     @Override
     public MappedStatement injectMappedStatement(Class<?> mapperClass, Class<?> modelClass, TableInfo tableInfo) {
-
-        return null;
+        SqlMethodEnum sqlMethod = SqlMethodEnum.UPDATE_ONE_SQL;
+        final String additional = optlockVersion(tableInfo) + tableInfo.getLogicDeleteSql(true, true);
+        // TODO sleepy
+        String sql = String.format(sqlMethod.getSql(),
+                tableInfo.getTableName(),
+                sqlSet(tableInfo.isWithLogicDelete(), false, tableInfo, false, ENTITY, ENTITY_DOT),
+                tableInfo.getKeyColumn(),
+                ENTITY_DOT + tableInfo.getKeyProperty(),
+                additional
+        );
+        SqlSource sqlSource = languageDriver.createSqlSource(configuration, sql, modelClass);
+        return addUpdateMappedStatement(mapperClass, modelClass, sqlSource);
     }
 }
--- stream-plugin/stream-plugin-mybatis-plus/src/test/java/io/github/vampireachao/stream/plugin/mybatisplus/MybatisPlusTestApplication.java
+++ stream-plugin/stream-plugin-mybatis-plus/src/test/java/io/github/vampireachao/stream/plugin/mybatisplus/MybatisPlusTestApplication.java
@@ -2,6 +2,7 @@ package io.github.vampireachao.stream.plugin.mybatisplus;
 
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import io.github.vampireachao.stream.plugin.mybatisplus.engine.annotation.EnableMybatisPlusPlugin;
 import org.mybatis.spring.annotation.MapperScan;
@@ -22,6 +23,7 @@ public class MybatisPlusTestApplication {
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
         interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.H2));
+        interceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
         return interceptor;
     }
 }
