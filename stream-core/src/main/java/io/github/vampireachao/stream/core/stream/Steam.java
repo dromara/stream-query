09c83f40cf5630f2660d031ce4aa1d2c679e732f
huangchengxing <huangchengxing@nova.net.cn>
2022-08-24 11:52:32 +0800

    允许增强Stream直接调用Collectors中的方法

--- stream-core/src/main/java/io/github/vampireachao/stream/core/stream/Steam.java
+++ stream-core/src/main/java/io/github/vampireachao/stream/core/stream/Steam.java
@@ -1,6 +1,5 @@
 package io.github.vampireachao.stream.core.stream;
 
-import io.github.vampireachao.stream.core.collector.Collective;
 import io.github.vampireachao.stream.core.lambda.function.SerBiCons;
 import io.github.vampireachao.stream.core.optional.Opp;
 
@@ -44,7 +43,7 @@ import java.util.stream.*;
  * @author VampireAchao
  * @see java.util.stream.Stream
  */
-public class Steam<T> implements Stream<T>, Iterable<T> {
+public class Steam<T> implements Stream<T>, Iterable<T>, CollectableStream<T> {
     /**
      * 代表不存在的下标, 一般用于并行流的下标, 或者未找到元素时的下标
      */
@@ -496,11 +495,11 @@ public class Steam<T> implements Stream<T>, Iterable<T> {
      * 扩散流操作，可能影响流元素个数，将原有流元素执行mapper操作，返回多个流所有元素组成的流，操作带一个方法，调用该方法可增加元素
      * 这是一个无状态中间操作
      *
-     * @param mapper 操作，返回流
      * @param <R>    拆分后流的元素类型
+     * @param mapper 操作，返回流
      * @return 返回叠加拆分操作后的流
      */
-    public <R> Steam<R> mapMulti(BiConsumer<? super T, ? super Builder<R>> mapper) {
+    public <R> Steam<R> mapMulti(BiConsumer<? super T, ? super Consumer<R>> mapper) {
         Objects.requireNonNull(mapper);
         return flatMap(e -> {
             Builder<R> buffer = Steam.builder();
@@ -1199,200 +1198,6 @@ public class Steam<T> implements Stream<T>, Iterable<T> {
     @Override
     public String toString() {
         return stream.toString();
-    }
-
-    /**
-     * 转换成集合
-     *
-     * @param collectionFactory 集合工厂(可以是集合构造器)
-     * @param <C>               集合类型
-     * @return 集合
-     */
-    public <C extends Collection<T>> C toColl(Supplier<C> collectionFactory) {
-        return collect(Collective.toCollection(collectionFactory));
-    }
-
-    /**
-     * 转换为ArrayList
-     *
-     * @return list
-     */
-    public List<T> toList() {
-        return collect(Collective.toList());
-    }
-
-    /**
-     * 转换为HashSet
-     *
-     * @return hashSet
-     */
-    public Set<T> toSet() {
-        return collect(Collective.toSet());
-    }
-
-    /**
-     * 与给定的可迭代对象转换成map，key为现有元素，value为给定可迭代对象迭代的元素<br>
-     * 至少包含全部的key，如果对应位置上的value不存在，则为null
-     *
-     * @param other 可迭代对象
-     * @param <R>   可迭代对象迭代的元素类型
-     * @return map，key为现有元素，value为给定可迭代对象迭代的元素;<br>
-     * 至少包含全部的key，如果对应位置上的value不存在，则为null;<br>
-     * 如果key重复, 则保留最后一个关联的value;<br>
-     */
-    public <R> Map<T, R> toZip(Iterable<R> other) {
-        // value对象迭代器
-        final Iterator<R> iterator = Opp.of(other).map(Iterable::iterator).orElseGet(Collections::emptyIterator);
-        if (isParallel()) {
-            List<T> keyList = toList();
-            final Map<T, R> map = new HashMap<>(keyList.size());
-            for (T key : keyList) {
-                map.put(key, iterator.hasNext() ? iterator.next() : null);
-            }
-            return map;
-        } else {
-            return toMap(Function.identity(), e -> iterator.hasNext() ? iterator.next() : null);
-        }
-    }
-
-    /**
-     * 返回拼接后的字符串
-     *
-     * @return 拼接后的字符串
-     */
-    public String join() {
-        return join("");
-    }
-
-    /**
-     * 返回拼接后的字符串
-     *
-     * @param delimiter 分隔符
-     * @return 拼接后的字符串
-     */
-    public String join(CharSequence delimiter) {
-        return join(delimiter, "", "");
-    }
-
-    /**
-     * 返回拼接后的字符串
-     *
-     * @param delimiter 分隔符
-     * @param prefix    前缀
-     * @param suffix    后缀
-     * @return 拼接后的字符串
-     */
-    public String join(CharSequence delimiter,
-                       CharSequence prefix,
-                       CharSequence suffix) {
-        return map(String::valueOf).collect(Collective.joining(delimiter, prefix, suffix));
-    }
-
-    /**
-     * 转换为map，key为给定操作执行后的返回值,value为当前元素
-     *
-     * @param keyMapper 指定的key操作
-     * @param <K>       key类型
-     * @return map
-     */
-    public <K> Map<K, T> toMap(Function<? super T, ? extends K> keyMapper) {
-        return toMap(keyMapper, Function.identity());
-    }
-
-    /**
-     * 转换为map，key,value为给定操作执行后的返回值
-     *
-     * @param keyMapper   指定的key操作
-     * @param valueMapper 指定value操作
-     * @param <K>         key类型
-     * @param <U>         value类型
-     * @return map
-     */
-    public <K, U> Map<K, U> toMap(Function<? super T, ? extends K> keyMapper,
-                                  Function<? super T, ? extends U> valueMapper) {
-        return toMap(keyMapper, valueMapper, (l, r) -> r);
-    }
-
-
-    /**
-     * 转换为map，key,value为给定操作执行后的返回值
-     *
-     * @param keyMapper     指定的key操作
-     * @param valueMapper   指定value操作
-     * @param mergeFunction 合并操作
-     * @param <K>           key类型
-     * @param <U>           value类型
-     * @return map
-     */
-    public <K, U> Map<K, U> toMap(Function<? super T, ? extends K> keyMapper,
-                                  Function<? super T, ? extends U> valueMapper,
-                                  BinaryOperator<U> mergeFunction) {
-        return toMap(keyMapper, valueMapper, mergeFunction, HashMap::new);
-    }
-
-    /**
-     * 转换为map，key,value为给定操作执行后的返回值
-     *
-     * @param keyMapper     指定的key操作
-     * @param valueMapper   指定value操作
-     * @param mergeFunction 合并操作
-     * @param mapSupplier   map工厂
-     * @param <K>           key类型
-     * @param <U>           value类型
-     * @param <M>           map类型
-     * @return map
-     */
-    public <K, U, M extends Map<K, U>> M toMap(Function<? super T, ? extends K> keyMapper,
-                                               Function<? super T, ? extends U> valueMapper,
-                                               BinaryOperator<U> mergeFunction,
-                                               Supplier<M> mapSupplier) {
-        return collect(Collective.toMap(keyMapper, valueMapper, mergeFunction, mapSupplier));
-    }
-
-
-    /**
-     * 通过给定分组依据进行分组
-     *
-     * @param classifier 分组依据
-     * @param <K>        实体中的分组依据对应类型，也是Map中key的类型
-     * @return {@link Collector}
-     */
-    public <K> Map<K, List<T>> group(Function<? super T, ? extends K> classifier) {
-        return group(classifier, Collective.toList());
-    }
-
-
-    /**
-     * 通过给定分组依据进行分组
-     *
-     * @param classifier 分组依据
-     * @param downstream 下游操作
-     * @param <K>        实体中的分组依据对应类型，也是Map中key的类型
-     * @param <D>        下游操作对应返回类型，也是Map中value的类型
-     * @param <A>        下游操作在进行中间操作时对应类型
-     * @return {@link Collector}
-     */
-    public <K, A, D> Map<K, D> group(Function<? super T, ? extends K> classifier,
-                                     Collector<? super T, A, D> downstream) {
-        return group(classifier, HashMap::new, downstream);
-    }
-
-    /**
-     * 通过给定分组依据进行分组
-     *
-     * @param classifier 分组依据
-     * @param mapFactory 提供的map
-     * @param downstream 下游操作
-     * @param <K>        实体中的分组依据对应类型，也是Map中key的类型
-     * @param <D>        下游操作对应返回类型，也是Map中value的类型
-     * @param <A>        下游操作在进行中间操作时对应类型
-     * @param <M>        最后返回结果Map类型
-     * @return {@link Collector}
-     */
-    public <K, D, A, M extends Map<K, D>> M group(Function<? super T, ? extends K> classifier,
-                                                  Supplier<M> mapFactory,
-                                                  Collector<? super T, A, D> downstream) {
-        return collect(Collective.groupingBy(classifier, mapFactory, downstream));
     }
 
     /**
