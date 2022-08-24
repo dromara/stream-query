387dfbe8891068f018942169cb419d569b79ab4e
VampireAchao <achao1441470436@gmail.com>
2022-08-24 22:32:05 +0800

    :trollface: autoConfig

--- stream-plugin/stream-plugin-mybatis-plus/src/main/java/io/github/vampireachao/stream/plugin/mybatisplus/engine/configuration/StreamPluginAutoConfiguration.java
+++ stream-plugin/stream-plugin-mybatis-plus/src/main/java/io/github/vampireachao/stream/plugin/mybatisplus/engine/configuration/StreamPluginAutoConfiguration.java
@@ -5,6 +5,7 @@ import com.baomidou.mybatisplus.core.injector.DefaultSqlInjector;
 import com.baomidou.mybatisplus.core.metadata.TableInfo;
 import io.github.vampireachao.stream.plugin.mybatisplus.engine.enumration.SqlMethodEnum;
 import io.github.vampireachao.stream.plugin.mybatisplus.engine.methods.InsertOneSql;
+import io.github.vampireachao.stream.plugin.mybatisplus.engine.methods.UpdateOneSql;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.annotation.Order;
@@ -27,6 +28,7 @@ public class StreamPluginAutoConfiguration {
             public List<AbstractMethod> getMethodList(Class<?> mapperClass, TableInfo tableInfo) {
                 List<AbstractMethod> methodList = super.getMethodList(mapperClass, tableInfo);
                 methodList.add(new InsertOneSql(SqlMethodEnum.INSERT_ONE_SQL.getMethod()));
+                methodList.add(new UpdateOneSql(SqlMethodEnum.UPDATE_ONE_SQL.getMethod()));
                 return methodList;
             }
         };
