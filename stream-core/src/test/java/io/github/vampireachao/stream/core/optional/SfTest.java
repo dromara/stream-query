a4b07122beb7fdf6211b2f6531aa2533cd9dc11a
臧臧 <2556450572@qq.com>
2022-09-17 10:56:46 +0800

    takeUnless,ofStr,of$Str :: bug SfTest

--- stream-core/src/test/java/io/github/vampireachao/stream/core/optional/SfTest.java
+++ stream-core/src/test/java/io/github/vampireachao/stream/core/optional/SfTest.java
@@ -1,6 +1,14 @@
 package io.github.vampireachao.stream.core.optional;
 
+import io.github.vampireachao.stream.core.lambda.function.SerSupp;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * @author VampireAchao
@@ -8,10 +16,150 @@ import org.junit.jupiter.api.Test;
  */
 class SfTest {
 
+    private final String ZVERIFY_NAME = "风向标";
+
     @Test
     void testOf() {
-        Sf.of(1).$let(i -> i + 1);
+        Sf<String> stringSf = Sf.of(ZVERIFY_NAME);
+        Assertions.assertEquals("风向标", stringSf.value);
     }
+
+    @Test
+    void testOfColl() {
+
+        Assertions.assertFalse(Sf.ofColl(Arrays.asList(null, null, null)).isEmpty());
+    }
+
+    @Test
+    void test$OfColl() {
+        Assertions.assertTrue(Sf.$ofColl(Arrays.asList(null, null, null)).isEmpty());
+    }
+
+    @Test
+    void testOfStr() {
+        Assertions.assertFalse(Sf.ofStr("  ").isEmpty());
+    }
+
+    @Test
+    void test$OfStr() {
+        Assertions.assertTrue(Sf.$ofStr("  ").isEmpty());
+    }
+
+    @Test
+    void testEmpty() {
+        Sf<Object> empty = Sf.empty();
+        Assertions.assertNull(empty.value);
+    }
+
+    @Test
+    void testIsEmpty() {
+        Assertions.assertFalse(Sf.ofStr("  ").isEmpty());
+    }
+
+    @Test
+    void testIsPresent() {
+        Assertions.assertTrue(Sf.ofStr("  ").isPresent());
+    }
+
+    @Test
+    void testLet() {
+        Sf<Integer> let = Sf.ofStr("  ").let(String::length);
+        Assertions.assertEquals(2, let.value);
+    }
+
+    @Test
+    void test$Let() {
+        Sf<String> stringSf = Sf.$ofStr(null).$let(a -> a.toString().length()).let(a -> ZVERIFY_NAME);
+        Assertions.assertEquals(ZVERIFY_NAME, stringSf.value);
+    }
+
+    @Test
+    void testAlso() {
+        AtomicReference<String> name = new AtomicReference<>(ZVERIFY_NAME);
+        Sf.of(null).also(a -> name.set("ZVerify"));
+        Assertions.assertEquals("ZVerify", name.get());
+    }
+
+    @Test
+    void test$Also() {
+        AtomicReference<String> name = new AtomicReference<>(ZVERIFY_NAME);
+        Sf.of(null).$also(a -> name.set("ZVerify"));
+        Assertions.assertEquals(ZVERIFY_NAME, name.get());
+    }
+
+    @Test
+    void testTakeIf() {
+        AtomicReference<String> name = new AtomicReference<>(ZVERIFY_NAME);
+        Sf.of(name).takeIf((a) -> Opp.ofStr(a.get()).isEmpty()).$also(a -> a.set("ZVerify"));
+        Assertions.assertEquals(ZVERIFY_NAME, name.get());
+    }
+
+    @Test
+    void test$TakeIf() {
+        AtomicReference<String> name = new AtomicReference<>(ZVERIFY_NAME);
+        Sf.of(name).$takeIf((a) -> Opp.ofStr(a.get()).isPresent()).also(a -> a.set("ZVerify"));
+        Assertions.assertEquals("ZVerify", name.get());
+    }
+
+    @Test
+    void testTakeUnless() {
+        AtomicReference<String> name = new AtomicReference<>(ZVERIFY_NAME);
+        Sf.of(name).takeUnless((a) -> Opp.ofStr(a.get()).isEmpty()).$also(a -> a.set("ZVerify"));
+        Assertions.assertEquals("ZVerify", name.get());
+    }
+
+    @Test
+    void test$TakeUnless() {
+        AtomicReference<String> name = new AtomicReference<>(ZVERIFY_NAME);
+        Sf.of(name.get()).$takeUnless(Objects::nonNull).$also(a -> name.set("ZVerify"));
+        Assertions.assertEquals(ZVERIFY_NAME, name.get());
+    }
+
+    @Test
+    void testIs() {
+        AtomicReference<String> name = new AtomicReference<>(ZVERIFY_NAME);
+        Assertions.assertTrue(Sf.of(name.get()).is(Objects::nonNull));
+    }
+
+    @Test
+    void testRequire() {
+        AtomicReference<String> name = new AtomicReference<>(null);
+        Sf<String> stringSf = Sf.ofStr(name.get());
+        Assertions.assertThrows(NoSuchElementException.class, stringSf::require);
+        Assertions.assertThrows(IllegalStateException.class, () -> stringSf.require(IllegalStateException::new));
+    }
+
+    @Test
+    void testOrThrow() {
+        AtomicReference<String> name = new AtomicReference<>(null);
+        Sf<String> stringSf = Sf.ofStr(name.get());
+        Assertions.assertThrows(NoSuchElementException.class, stringSf::orThrow);
+        Assertions.assertThrows(IllegalStateException.class, () -> stringSf.orThrow(IllegalStateException::new));
+    }
+
+
+
+    @Test
+    void testOr() {
+        AtomicReference<String> name = new AtomicReference<>(null);
+        Sf<String> or = Sf.ofStr(name.get()).or(() -> Sf.ofStr(ZVERIFY_NAME));
+        Assertions.assertEquals(ZVERIFY_NAME, or.get());
+    }
+
+    @Test
+    void testOrGet() {
+        AtomicReference<String> name = new AtomicReference<>(null);
+        String orGet = Sf.ofStr(name.get()).orGet(() -> ZVERIFY_NAME);
+        Assertions.assertEquals(ZVERIFY_NAME, orGet);
+    }
+
+    @Test
+    void testOrElse() {
+        AtomicReference<String> name = new AtomicReference<>(null);
+        String orElse = Sf.ofStr(name.get()).orElse(ZVERIFY_NAME);
+        Assertions.assertEquals(ZVERIFY_NAME, orElse);
+    }
+
 
 
 }
