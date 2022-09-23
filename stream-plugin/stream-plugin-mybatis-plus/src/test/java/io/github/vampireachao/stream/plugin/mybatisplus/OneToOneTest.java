cfc47e096f4d797661ca59e0fa9c7843bf3683d6
VampireAchao <achao1441470436@gmail.com>
2022-09-23 18:05:49 +0800

    :trollface: quick save

--- stream-plugin/stream-plugin-mybatis-plus/src/test/java/io/github/vampireachao/stream/plugin/mybatisplus/OneToOneTest.java
+++ stream-plugin/stream-plugin-mybatis-plus/src/test/java/io/github/vampireachao/stream/plugin/mybatisplus/OneToOneTest.java
@@ -41,6 +41,29 @@ class OneToOneTest {
 
         Map<Long, Boolean> query = OneToOne.query(w -> w.select(UserInfo::getId, UserInfo::getName), userIds, UserInfo::getId, userInfo -> userInfo.getName() != null && userInfo.getName().contains("a"));
         Assertions.assertEquals(2, query.values().stream().filter(Boolean::booleanValue).count());
+
+        /*List<Long> userIds = Arrays.asList(1L, 2L, 3L, 4L, 5L);
+        Map<Long, UserInfo> idUserMap = OneToOne.of(UserInfo::getId).in(userIds).query();
+        Assertions.assertEquals(5, idUserMap.size());
+
+        Map<Long, String> userIdNameMap = OneToOne.of(UserInfo::getId).in(userIds).value(UserInfo::getName).query();
+        Assertions.assertEquals(5, userIdNameMap.size());
+
+        userIdNameMap = OneToOne.of(UserInfo::getId).in(userIds).value(UserInfo::getName).condition(w -> w.le(UserInfo::getAge, 22)).query();
+        Assertions.assertEquals(3, userIdNameMap.size());
+
+
+        idUserMap = OneToOne.of(UserInfo::getId).eq(1L).query();
+        Assertions.assertEquals(1, idUserMap.size());
+
+        userIdNameMap = OneToOne.of(UserInfo::getId).eq(1L).value(UserInfo::getName).query();
+        Assertions.assertEquals(1, userIdNameMap.size());
+
+        userIdNameMap = OneToOne.of(UserInfo::getId).eq(1L).value(UserInfo::getName).condition(w -> w.le(UserInfo::getAge, 22)).query();
+        Assertions.assertEquals(1, userIdNameMap.size());
+
+        Map<Long, Boolean> query = OneToOne.of(UserInfo::getId).eq(1L).condition(w -> w.select(UserInfo::getId, UserInfo::getName)).value(userInfo -> userInfo.getName() != null && userInfo.getName().contains("a")).query();
+        Assertions.assertEquals(2, query.values().stream().filter(Boolean::booleanValue).count());*/
     }
 
 }
