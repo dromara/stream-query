086ac32e792f30143daa628e5741336ac578ce1e
VampireAchao <achao1441470436@gmail.com>
2022-09-29 09:47:11 +0800

    :trollface: test case

--- stream-plugin/stream-plugin-mybatis-plus/src/test/java/io/github/vampireachao/stream/plugin/mybatisplus/OneToManyToOneTest.java
+++ stream-plugin/stream-plugin-mybatis-plus/src/test/java/io/github/vampireachao/stream/plugin/mybatisplus/OneToManyToOneTest.java
@@ -28,34 +28,28 @@ class OneToManyToOneTest {
 
     @Test
     void testQuery() {
-        Assertions.assertAll(() -> {
-            List<UserInfo> userInfos = SimpleQuery.selectList(UserInfo.class, Wrappers.lambdaQuery());
-            Set<Long> userIds = Steam.of(userInfos).map(UserInfo::getId).toSet();
+        List<UserInfo> userInfos = SimpleQuery.selectList(UserInfo.class, Wrappers.lambdaQuery());
+        Set<Long> userIds = Steam.of(userInfos).map(UserInfo::getId).toSet();
 
-            List<String> roleIds = new ArrayList<>();
-            Map<Long, List<String>> userIdRoleIds = OneToMany.of(UserRole::getUserId).in(userIds).value(UserRole::getRoleId).peek(e -> roleIds.add(e.getRoleId())).query();
-            Map<String, RoleInfo> idRoleMap = OneToOne.of(RoleInfo::getId).in(roleIds).query();
-            Map<Long, List<RoleInfo>> userIdRolesMap = Steam.of(userIdRoleIds.entrySet()).map(e -> MapUtil.entry(e.getKey(), Steam.of(e.getValue()).map(idRoleMap::get).nonNull().toList())).collect(Collective.entryToMap());
-            Assertions.assertEquals(5, userIdRolesMap.size());
+        List<String> roleIds = new ArrayList<>();
+        Map<Long, List<String>> userIdRoleIds = OneToMany.of(UserRole::getUserId).in(userIds).value(UserRole::getRoleId).peek(e -> roleIds.add(e.getRoleId())).query();
+        Map<String, RoleInfo> idRoleMap = OneToOne.of(RoleInfo::getId).in(roleIds).query();
+        Map<Long, List<RoleInfo>> userIdRolesMap = Steam.of(userIdRoleIds.entrySet()).map(e -> MapUtil.entry(e.getKey(), Steam.of(e.getValue()).map(idRoleMap::get).nonNull().toList())).collect(Collective.entryToMap());
+        Assertions.assertEquals(5, userIdRolesMap.size());
 
-            /*Map<Long, List<RoleInfo>> userIdRoleInfosMap = OneToManyToOne.of(UserRole::getUserId).in(userIds).value(UserRole::getRoleId)
-                    .attachKey(RoleInfo::getId).attachPeek(SerCons.nothing()).query();
-            Assertions.assertEquals(5, userIdRoleInfosMap.size());
+        /*Map<Long, List<RoleInfo>> userIdRoleInfosMap = OneToManyToOne.of(UserRole::getUserId).in(userIds).value(UserRole::getRoleId)
+                .attachKey(RoleInfo::getId).attachPeek(SerCons.nothing()).query();
+        Assertions.assertEquals(5, userIdRoleInfosMap.size());
 
-            Map<String, List<String>> roleIdNamesMap = OneToManyToOne.of(UserRole::getUserId).in(userIds).value(UserRole::getRoleId)
-                    .attachKey(RoleInfo::getId).attachValue(RoleInfo::getRoleName).groupBy(UserRole::getRoleId).query(Collective.toList());
-            Assertions.assertFalse(roleIdNamesMap.isEmpty());*/
+        Map<String, List<String>> roleIdNamesMap = OneToManyToOne.of(UserRole::getUserId).in(userIds).value(UserRole::getRoleId)
+                .attachKey(RoleInfo::getId).attachValue(RoleInfo::getRoleName).groupBy(UserRole::getRoleId).query(Collective.toList());
+        Assertions.assertFalse(roleIdNamesMap.isEmpty());*/
 
-            /*OneToMany.of(UserRole::getUserId).in(userIds).value(UserRole::getRoleId)
-                    .join((Map<Long, List<String>> userIdRoleIdsMap) -> OneToOne.of(RoleInfo::getId).in(Steam.of(userIdRoleIdsMap.values()).flat(SerFunc.identity()).toList()),
-                            (Map<Long, List<String>> userIdRoleIdsMap, Map<String, RoleInfo> idRoleMap) ->
-                                    Steam.of(userIdRoleIdsMap.entrySet()).map(e -> MapUtil.entry(e.getKey(), Steam.of(e.getValue()).map(idRoleMap::get).nonNull().toList())).collect(Collective.entryToMap())).query();*/
+        /*OneToMany.of(UserRole::getUserId).in(userIds).value(UserRole::getRoleId)
+                .join((Map<Long, List<String>> userIdRoleIdsMap) -> OneToOne.of(RoleInfo::getId).in(Steam.of(userIdRoleIdsMap.values()).flat(SerFunc.identity()).toList()),
+                        (Map<Long, List<String>> userIdRoleIdsMap, Map<String, RoleInfo> idRoleMap) ->
+                                Steam.of(userIdRoleIdsMap.entrySet()).map(e -> MapUtil.entry(e.getKey(), Steam.of(e.getValue()).map(idRoleMap::get).nonNull().toList())).collect(Collective.entryToMap())).query();*/
 
-            Map<Long, List<RoleInfo>> userIdRolesJoinOnMap = OneToManyToOne.of(UserRole::getUserId).in(userIds).value(UserRole::getRoleId).attachKey(RoleInfo::getId).query();
-
-            Map<String, List<RoleInfo>> roleIdRolesJoinOnMap = OneToManyToOne.of(UserRole::getUserId).in(userIds).value(UserRole::getRoleId).attachKey(RoleInfo::getId).groupBy(UserRole::getRoleId).query();
-
-        });
     }
 
 }
