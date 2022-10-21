e5cf3dd9a865dd7169d5eeee2aca9455b9215db8
臧臧 <2556450572@qq.com>
2022-10-21 22:10:12 +0800

    Supplement doc upup

--- stream-core/src/main/java/io/github/vampireachao/stream/core/collection/Lists.java
+++ stream-core/src/main/java/io/github/vampireachao/stream/core/collection/Lists.java
@@ -1,28 +1,74 @@
 package io.github.vampireachao.stream.core.collection;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import io.github.vampireachao.stream.core.stream.Steam;
+
+import java.util.*;
 
 /**
- * @author VampireAchao
+ * @author VampireAchao 请我吃早饭
  * @since 2022/10/21 17:08
  */
 public class Lists {
 
+    /**
+     *
+     */
     private Lists() {
         /* Do not new me! */
     }
 
+    /**
+     * @param values 集合元素
+     * @return {@link List}<{@link T}>
+     */
     @SafeVarargs
     public static <T> List<T> of(T... values) {
         return new ArrayList<>(Arrays.asList(values));
     }
 
+    /**
+     * 翻转集合元素
+     *
+     * @param list 需翻转集合元素
+     * @return {@link List}<{@link T}> 翻转之后集合元素
+     */
     public static <T> List<T> reverse(List<T> list) {
         Collections.reverse(list);
         return list;
     }
+
+    /**
+     * 升序排序
+     *
+     * @param list 需排序集合
+     * @return {@link List}<{@link T}> 排序后集合
+     */
+    public static <T> List<T> ascend(List<T> list) {
+        list.sort(null);
+        return list;
+    }
+
+    /**
+     * 降序排序
+     *
+     * @param list 需排序集合
+     * @return {@link List}<{@link T}> 排序后集合
+     */
+    public static <T> List<T> descend(List<T> list) {
+        list.sort(Collections.reverseOrder());
+        return list;
+    }
+
+    /**
+     * 查找指定集合中的元素，如不存在返回-1
+     * @param list 要查询集合
+     * @param objectives 要查找元素
+     * @return {@link Integer} 所查找元素的索引，如不存在返回-1
+     */
+    public static <T> Integer binarySearch(List<T> list,T objectives) {
+
+       return Steam.of(list).findFirstIdx((value) -> value == objectives);
+    }
+
 
 }
