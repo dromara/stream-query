8a12931e7b1fbfac167ad1d4ecdd550e6ca3e576
VampireAchao <achao1441470436@gmail.com>
2022-11-14 14:22:37 +0800

    :trollface: exception

--- stream-core/src/main/java/io/github/vampireachao/stream/core/lambda/LambdaInvokeException.java
+++ stream-core/src/main/java/io/github/vampireachao/stream/core/lambda/LambdaInvokeException.java
@@ -18,6 +18,18 @@ public class LambdaInvokeException extends RuntimeException {
     }
 
     /**
+     * Constructs a new runtime exception with the specified detail message.
+     * The cause is not initialized, and may subsequently be initialized by a
+     * call to {@link #initCause}.
+     *
+     * @param message the detail message. The detail message is saved for
+     *                later retrieval by the {@link #getMessage()} method.
+     */
+    public LambdaInvokeException(String message) {
+        super(message);
+    }
+
+    /**
      * getRealException
      *
      * @return realException
