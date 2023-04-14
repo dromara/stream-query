e1c8ae70f302b1532bfa08ea8563d6813071af58
KamTo Hung <kamtohung@gmail.com>
2023-04-14 15:06:10 +0800

    feature(#https://gitee.com/dromara/stream-query/issues/I6VGN0)

--- stream-plugin/stream-plugin-mybatis-plus/src/test/java/org/dromara/streamquery/stream/plugin/mybatisplus/pojo/po/package-info.java
+++ stream-plugin/stream-plugin-mybatis-plus/src/test/java/org/dromara/streamquery/stream/plugin/mybatisplus/pojo/po/package-info.java
@@ -15,9 +15,3 @@
  * limitations under the License.
  */
 package org.dromara.streamquery.stream.plugin.mybatisplus.pojo.po;
-
-/**
- * @author <a href = "kamtohung@gmail.com">KamTo Hung</a>
- */
-public interface ParentScan {
-}
