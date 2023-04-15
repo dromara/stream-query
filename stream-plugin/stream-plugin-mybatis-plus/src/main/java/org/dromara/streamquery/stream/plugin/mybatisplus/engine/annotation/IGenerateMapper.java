50f6cc6922b9e07589dad5ebbd925f7231c57542
KamTo Hung <kamtohung@gmail.com>
2023-04-15 10:41:45 +0800

    feature(#https://gitee.com/dromara/stream-query/issues/I6VGN0)

--- stream-plugin/stream-plugin-mybatis-plus/src/main/java/org/dromara/streamquery/stream/plugin/mybatisplus/engine/annotation/IGenerateMapper.java
+++ stream-plugin/stream-plugin-mybatis-plus/src/main/java/org/dromara/streamquery/stream/plugin/mybatisplus/engine/annotation/IGenerateMapper.java
@@ -14,29 +14,12 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.dromara.streamquery.stream.plugin.mybatisplus.pojo.po;
-
-import com.baomidou.mybatisplus.annotation.DbType;
-import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.KeySequence;
-import com.baomidou.mybatisplus.annotation.TableId;
-import lombok.Data;
-import org.dromara.streamquery.stream.plugin.mybatisplus.annotation.Entity;
-import org.dromara.streamquery.stream.plugin.mybatisplus.engine.annotation.GenerateMapper;
+package org.dromara.streamquery.stream.plugin.mybatisplus.engine.annotation;
 
 /**
- * RoleInfo
+ * Generate Mapper Interface
  *
- * @author VampireAchao Cizai_
- * @since 2022/5/23
+ * @author <a href = "kamtohung@gmail.com">KamTo Hung</a>
  */
-@Data
-@GenerateMapper
-@KeySequence(dbType = DbType.H2)
-public class RoleInfo {
-
-  @TableId(type = IdType.ASSIGN_ID)
-  private String id;
-
-  private String roleName;
+public interface IGenerateMapper {
 }
