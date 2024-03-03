400387ef216db031ca51013499b713dddba5b1db
VampireAchao <achao@apache.org>
2024-03-03 16:37:48 +0800

    [improve] add dsl and ddl step for mysql/pgsql ci

---
+++ stream-plugin/stream-plugin-mybatis-plus/src/test/resources/sql/pgsql/data.sql
@@ -0,0 +1,52 @@
+DELETE
+FROM user_info;
+INSERT INTO user_info (name, age, email)
+VALUES ('Jon', 18, 'myEmail1'),
+       ('Jack', 18, 'myEmail2'),
+       ('Tom', 28, 'myEmail3'),
+       ('Sandy', 21, 'myEmail4'),
+       ('Billie', 24, 'myEmail5');
+
+DELETE
+FROM user_role;
+INSERT INTO user_role (user_id, role_id)
+VALUES (1, '1'),
+       (1, '2'),
+       (2, '1'),
+       (2, '3'),
+       (3, '1'),
+       (4, '1'),
+       (4, '2'),
+       (5, '1'),
+       (5, '2'),
+       (5, '3');
+
+DELETE
+FROM role_info;
+INSERT INTO role_info (id, role_name)
+VALUES ('1', 'admin'),
+       ('2', 'user'),
+       ('3', 'guest');
+
+DELETE
+FROM product_info;
+INSERT INTO product_info (product_name, product_price, tenant_id)
+VALUES ('Apple iPhone 13', 699, '1'),
+       ('Samsung Galaxy S21', 799, '1'),
+       ('OnePlus 9 Pro', 969, '1'),
+       ('Google Pixel 6', 599, '2'),
+       ('Sony Xperia 1 III', 1199, '1');
+
+DELETE
+FROM product_category;
+INSERT INTO product_category (product_id, category_id, tenant_id)
+VALUES (1, 1, '1'),
+       (1, 2, '1'),
+       (2, 1, '1'),
+       (2, 3, '1'),
+       (3, 1, '1'),
+       (4, 1, '2'),
+       (4, 2, '2'),
+       (5, 1, '1'),
+       (5, 2, '1'),
+       (5, 3, '1');
---
+++ stream-plugin/stream-plugin-mybatis-plus/src/test/resources/sql/pgsql/schema.sql
@@ -0,0 +1,44 @@
+DROP TABLE IF EXISTS user_info;
+CREATE TABLE IF NOT EXISTS user_info
+(
+    id          BIGSERIAL PRIMARY KEY NOT NULL,
+    name        VARCHAR(50) DEFAULT NULL,
+    age         INT         DEFAULT NULL,
+    email       VARCHAR(50) DEFAULT NULL,
+    gmt_deleted TIMESTAMP   DEFAULT '2001-01-01 00:00:00',
+    PRIMARY KEY (id)
+    );
+
+DROP TABLE IF EXISTS user_role;
+CREATE TABLE IF NOT EXISTS user_role
+(
+    id      BIGSERIAL PRIMARY KEY NOT NULL,
+    user_id BIGINT      DEFAULT NULL,
+    role_id VARCHAR(30) DEFAULT NULL
+    );
+
+DROP TABLE IF EXISTS role_info;
+CREATE TABLE IF NOT EXISTS role_info
+(
+    id        VARCHAR(30) NOT NULL,
+    role_name VARCHAR(30) DEFAULT NULL,
+    PRIMARY KEY (id)
+    );
+
+DROP TABLE IF EXISTS product_info;
+CREATE TABLE IF NOT EXISTS product_info
+(
+    id            BIGSERIAL PRIMARY KEY NOT NULL,
+    product_name  VARCHAR(50) DEFAULT NULL,
+    product_price FLOAT       DEFAULT NULL,
+    tenant_id     VARCHAR(50) DEFAULT NULL
+    );
+
+DROP TABLE IF EXISTS product_category;
+CREATE TABLE IF NOT EXISTS product_category
+(
+    id          BIGSERIAL PRIMARY KEY NOT NULL,
+    product_id  BIGINT DEFAULT NULL,
+    category_id BIGINT DEFAULT NULL,
+    tenant_id   VARCHAR(50) DEFAULT NULL
+    );
