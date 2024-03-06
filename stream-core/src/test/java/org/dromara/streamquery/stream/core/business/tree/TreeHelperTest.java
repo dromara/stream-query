3f378c535f710fc8653d0c50861056bc642539ba
zang <zang@dromara.org>
2024-03-06 15:50:40 +0800

    新增类似前端级联选择器功能

--- stream-core/src/test/java/org/dromara/streamquery/stream/core/business/tree/TreeHelperTest.java
+++ stream-core/src/test/java/org/dromara/streamquery/stream/core/business/tree/TreeHelperTest.java
@@ -25,8 +25,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static java.util.Arrays.asList;
 import static java.util.Collections.singletonList;
@@ -183,6 +185,20 @@ class TreeHelperTest {
     Assertions.assertEquals(originStudentTree, studentTree);
   }
 
+  @Test
+  void testCascadeSelect() {
+    List<Student> studentTree = studentTreeHelper.toTree(originStudentList);
+
+    Long targetStudentId = 6L;
+
+    List<Long> expectedPathIds = Arrays.asList(1L, 3L, 6L);
+
+    List<Student> selectedPath = studentTreeHelper.cascadeSelect(studentTree, targetStudentId);
+
+    List<Long> selectedPathIds = selectedPath.stream().map(Student::getId).collect(Collectors.toList());
+
+    Assertions.assertEquals(expectedPathIds, selectedPathIds);
+  }
   @Test
   void testToTreeWithCondition() {
     TreeHelper<Student, Long> conditionTreeHelper =
