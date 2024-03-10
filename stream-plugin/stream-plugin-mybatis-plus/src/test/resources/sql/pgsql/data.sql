6ae46c5734f680508521ea231fe1cbd8de6359ef
VampireAchao <achao@apache.org>
2024-03-10 21:07:41 +0800

    [ci] fix pgsql dsl and ddl

--- stream-plugin/stream-plugin-mybatis-plus/src/test/resources/sql/pgsql/data.sql
+++ stream-plugin/stream-plugin-mybatis-plus/src/test/resources/sql/pgsql/data.sql
@@ -1,6 +1,6 @@
 DELETE
-FROM user_info;
-INSERT INTO user_info (name, age, email)
+FROM "user_info";
+INSERT INTO "user_info" (name, age, email)
 VALUES ('Jon', 18, 'myEmail1'),
        ('Jack', 18, 'myEmail2'),
        ('Tom', 28, 'myEmail3'),
@@ -8,8 +8,8 @@ VALUES ('Jon', 18, 'myEmail1'),
        ('Billie', 24, 'myEmail5');
 
 DELETE
-FROM user_role;
-INSERT INTO user_role (user_id, role_id)
+FROM "user_role";
+INSERT INTO "user_role" (user_id, role_id)
 VALUES (1, '1'),
        (1, '2'),
        (2, '1'),
@@ -22,24 +22,22 @@ VALUES (1, '1'),
        (5, '3');
 
 DELETE
-FROM role_info;
-INSERT INTO role_info (id, role_name)
+FROM "role_info";
+INSERT INTO "role_info" (id, role_name)
 VALUES ('1', 'admin'),
        ('2', 'user'),
        ('3', 'guest');
 
-DELETE
-FROM product_info;
-INSERT INTO product_info (product_name, product_price, tenant_id)
+DELETE FROM "product_info";
+INSERT INTO "product_info" (product_name, product_price, tenant_id)
 VALUES ('Apple iPhone 13', 699, '1'),
        ('Samsung Galaxy S21', 799, '1'),
        ('OnePlus 9 Pro', 969, '1'),
        ('Google Pixel 6', 599, '2'),
        ('Sony Xperia 1 III', 1199, '1');
 
-DELETE
-FROM product_category;
-INSERT INTO product_category (product_id, category_id, tenant_id)
+DELETE FROM "product_category";
+INSERT INTO "product_category" (product_id, category_id, tenant_id)
 VALUES (1, 1, '1'),
        (1, 2, '1'),
        (2, 1, '1'),
